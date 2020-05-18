1af130855877d4dbf9157129869fd800c5f52835
David Noone <david145noone@gmail.com>
2020-05-18 11:44:13 +0100

    Started fileReader

---
+++ src/main/java/computer_science/FileReader.java
@@ -0,0 +1,20 @@
+package computer_science;
+import java.io.*;
+
+public class FileReader {
+
+    public FileReader() {
+
+    }
+
+    public void readData() {
+
+        try {
+
+        } finally {
+
+        }
+    }
+
+
+}
