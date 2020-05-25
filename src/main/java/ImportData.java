e314f87e989bd293b17daab5bbf13e0a26417ca7
David Noone <david145noone@gmail.com>
2020-05-25 23:45:25 +0100

    Working out classes and functions

--- src/main/java/ImportData.java
+++ src/main/java/ImportData.java
@@ -2,10 +2,29 @@
 idea: so ImportData is supposed to try to import each line
  of StudentInfo, and create a HashMap of key- values, using sNum as key
 */
-
 import java.io.*;
+import java.util.*;
 
 public class ImportData {
 
+    private String filePath;
+    private HashMap<Integer, Student> students;
 
+    public ImportData(String filePath) {
+        this.filePath = filePath;
+        students = new HashMap<>();
+    }
+
+    public void openFile() {
+
+    }
+
+    // for each line of the file we want to add a student to the hashmap
+    public void addStudent() {
+
+    }
+
+    public void closeFile() {
+
+    }
 }
